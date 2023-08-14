613fdf7ed8a855908e16883eccc394652896adfe
Hichu187 <longdam1312@gmail.com>
2023-08-14 17:32:21 +0700

    pathfinding

--- Assets/_Scripts/EnemyMovement.cs
+++ Assets/_Scripts/EnemyMovement.cs
@@ -14,6 +14,8 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(FollowTarget());
     }
 
     // Update is called once per frame
